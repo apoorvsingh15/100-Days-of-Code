56377c7643530b3e1817619262c5430459111eb4
Apoorv Singh <apoorv.singh15@gmail.com>
2024-01-15 17:51:06 +0530

    :sparkles: solved Arrays - Contains Duplicates

---
+++ Day 1/ContainsDuplicate.js
@@ -0,0 +1,36 @@
+
+// N2 - Brute Force Method
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// var containsDuplicate = function (nums) {
+//   // sort the array
+//   if (nums.length > 1) {
+//     const sorted = nums.sort((a, b) => a - b);
+
+//     for (let i = 0; i < sorted.length; i++) {
+//       if (sorted[i] === sorted[i + 1]) {
+//         return true;
+//       }
+//       return false
+//     }
+//   } else { return false };
+// };
+
+// Using HashMap - O(n)
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const numbers = new Set(nums);
+  const isEqual = numbers.size === nums.length;
+  return !isEqual;
+};
+
+
+
+
+
+console.log(containsDuplicate([1, 2, 3, 4]));
